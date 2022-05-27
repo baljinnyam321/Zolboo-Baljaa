1a9cea0e72ca4153e0431f2e0feb2da8b6deb98d
baljinnyam321 <106389367+baljinnyam321@users.noreply.github.com>
2022-05-27 21:39:39 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "Хайрт чамдаа мэндэлсэн өдрийн гал халуун мэндийг дэвшүүлье.mp4");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/c1K8bnLZxBo");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
